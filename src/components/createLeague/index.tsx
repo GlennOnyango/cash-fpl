3e9b87b17cd94c2932b6b69ce5cdd85dd1dcc691
GlennOnyango <glennonyango@yahoo.com>
2024-06-10 09:56:46 +0300

    Create league done

--- src/components/createLeague/index.tsx
+++ src/components/createLeague/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import {
   Button,
   CheckboxGroup,
@@ -7,10 +7,12 @@ import {
   Select,
   SelectItem,
   Checkbox,
+  Avatar,
 } from "@nextui-org/react";
 import { useFormState } from "react-dom";
 import { createLeague } from "@/app/actions";
 import Link from "next/link";
+import { CameraIcon } from "../icons/CameraIcon";
 
 export const access = [
   { key: "public", label: "Public" },
@@ -25,6 +27,17 @@ export default function CreateLeagueComponent() {
   const [state, formAction] = useFormState(createLeague, initialState);
   const [selected, setSelected] = React.useState(["weekly"]);
   const [rules, rulesSelected] = React.useState(["tranasction-fine"]);
+  const [ur, setUr] = React.useState<string>(
+    "https://images.unsplash.com/broken"
+  );
+
+  const handleInputChange = (e: any) => {
+    const { name } = e.target;
+    if (e.target.files[0] !== undefined) {
+      setUr(URL.createObjectURL(e.target.files[0]));
+    }
+  };
+
   return (
     <form
       action={formAction}
@@ -36,38 +49,49 @@ export default function CreateLeagueComponent() {
     >
       <div className="col-span-3 flex justify-center items-center">
         <label
-          style={{ width: "300px", height: "300px" }}
+          style={{ width: "200px", height: "200px" }}
           className=" flex flex-col items-center justify-center border-2
-         border-gray-300 border-dashed rounded-full cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+         border-gray-900  rounded-full cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
         >
-          <div className="flex flex-col items-center justify-center pt-5 pb-6">
-            <svg
-              className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 16"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-              />
-            </svg>
-            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-              <span className="font-semibold">Click to upload</span> or drag and
-              drop
-            </p>
-            <p className="text-xs text-gray-500 dark:text-gray-400">
-              SVG, PNG, JPG or GIF (MAX. 800x400px)
-            </p>
+          <div className=" flex flex-col items-center justify-center pt-5 pb-6">
+            <Avatar
+              showFallback={true}
+              style={{ width: "200px", height: "200px" }}
+              src={ur}
+              className="bg-gray-800"
+              fallback={
+                <div className=" flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    className="w-8 h-8 mb-4 text-white dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                    />
+                  </svg>
+                  <p className="mb-2 text-sm text-white dark:text-gray-400">
+                    <span className="font-semibold">League Logo</span>
+                  </p>
+                  <p className="text-xs text-white dark:text-gray-400">
+                    PNG or JPG (MAX. 800x400px)
+                  </p>
+                </div>
+              }
+            />
           </div>
           <input
             id="dropzone-file"
             name="avatar"
             type="file"
+            accept="image/*"
+            onChange={handleInputChange}
             className="hidden"
           />
         </label>
