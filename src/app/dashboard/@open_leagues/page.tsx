8cccad4cc8a1e690fa074d41cee20c7f2c70c0f6
GlennOnyango <glennonyango@yahoo.com>
2024-06-23 21:36:59 +0300

    Curved out leagues structure

--- src/app/dashboard/@open_leagues/page.tsx
+++ src/app/dashboard/@open_leagues/page.tsx
@@ -2,7 +2,7 @@ import AppComplexLeague from "./components/App";
 
 export default function Leagues() {
   return (
-    <div className="col-span-12 sm:col-span-4 row-span-6 bg-white p-4 rounded-md">
+    <div className="col-span-12 sm:col-span-4 row-span-6 bg-white p-4 rounded-none">
       <h4 className="text-3xl text-black/90 dark:text-white/90 mb-4">
         Open Leagues
       </h4>
